cfc08699ee88ec91dada872bd34ed74eb5d3af08
Llewelyn Fernandes <34630995+lewfer@users.noreply.github.com>
2026-06-08 09:49:56 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
 namespace bitmake {
     //% blockId=setButton
-    //% block="create a button on pin %pin"
+    //% block="button on pin %pin"
     //% weight=50
     export function setButton(pin: DigitalPin) {
         pins.setPull(pin, PinPullMode.PullUp)
     }
 
     //% blockId=buttonPressed
-    //% block="check if button on pin %pin is pressed"
+    //% block="button %pin pressed"
     //% weight=50
     export function buttonPressed(pin: DigitalPin) {
         return pins.digitalReadPin(pin)==0            
     }
+
+    //% blockId=buttonWaitForPress
+    //% block="wait for button %pin pressed"
+    //% weight=50
+    export function buttonWaitForPress(pin: DigitalPin) {
+        while (pins.digitalReadPin(pin) != 0)
+            basic.pause(100)
+    }
+
 
 }

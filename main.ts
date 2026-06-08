6b1ff524d1153beb39240b9bf45a3492e9e1db9c
Llewelyn Fernandes <34630995+lewfer@users.noreply.github.com>
2026-06-08 09:59:01 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
+//% color="#ff7f50" icon="\uf06e" block="BitMake"
 namespace bitmake {
     //% blockId=setButton
     //% block="button on pin %pin"
     //% weight=50
+    //% group="Button"
     export function setButton(pin: DigitalPin) {
         pins.setPull(pin, PinPullMode.PullUp)
     }
@@ -9,6 +11,7 @@ namespace bitmake {
     //% blockId=buttonPressed
     //% block="button %pin pressed"
     //% weight=50
+    //% group="Button"
     export function buttonPressed(pin: DigitalPin) {
         return pins.digitalReadPin(pin)==0            
     }
@@ -16,10 +19,41 @@ namespace bitmake {
     //% blockId=buttonWaitForPress
     //% block="wait for button %pin pressed"
     //% weight=50
+    //% group="Button"
     export function buttonWaitForPress(pin: DigitalPin) {
         while (pins.digitalReadPin(pin) != 0)
             basic.pause(100)
     }
 
+    //% blockId=buttonWaitForPress
+    //% block="wait for button %pin released"
+    //% weight=50
+    //% group="Button"
+    export function buttonWaitForRelease(pin: DigitalPin) {
+        while (pins.digitalReadPin(pin) == 0)
+            basic.pause(100)
+    }
 
+    //% blockId=setLed
+    //% block="led on pin %pin"
+    //% weight=50
+    //% group="Led"
+    export function setLed(pin: DigitalPin) {
+    }
+
+    //% blockId=ledOn
+    //% block="led %pin on"
+    //% weight=50
+    //% group="Led"
+    export function ledOn(pin: DigitalPin) {
+        pins.digitalWritePin(pin, 1)
+    }
+
+    //% blockId=ledOff
+    //% block="led %pin off"
+    //% weight=50
+    //% group="Led"
+    export function ledOff(pin: DigitalPin) {
+        pins.digitalWritePin(pin, 0)
+    }
 }

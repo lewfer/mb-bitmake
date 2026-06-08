3da47d484df9a4802324088f3d794c500fa5df4e
Llewelyn Fernandes <34630995+lewfer@users.noreply.github.com>
2026-06-08 09:41:12 +0100

    button

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
-})
+namespace bitmake {
+    //% blockId=setButton
+    //% block="create a button on pin %pin"
+    //% weight=50
+    export function setButton(pin: DigitalPin) {
+        pins.setPull(pin, PinPullMode.PullUp)
+    }
+
+    //% blockId=setButton
+    //% block="create a button on pin %pin"
+    //% weight=50
+    export function buttonPressed(pin: DigitalPin) {
+        return pins.digitalReadPin(pin)==0            
+    }
+
+}
